b0e9b4daa16bd274efd7e1f1f7b8e994df7168f7
Jonathan Chung <jwbomt@gmail.com>
2015-02-17 14:28:31 -0500

    Consolidate fisher's method.
    Move rank statistic calculation to seperate file because it is called by both ARTP and Fisher's method.

--- R/calculate_permutation_pvalues.R
+++ R/calculate_permutation_pvalues.R
@@ -1,3 +1,15 @@
+#' Count the number of elements in \code{a} are less than \code{b}
+#'
+#' @param a thing that less
+#' @param b thing that is more
+#' @return \code{integer} count of times \code{a} is less than \code{b}
+#' @export
+count_a_less_than_b <- function(a, b){
+  count_of_a_less_than_b <- a <= b
+  count_of_a_less_than_b <- sum(as.integer(count_of_a_less_than_b))
+  return(count_of_a_less_than_b)
+}
+
 #' Calculate empirical p-values from permutation data.
 #'
 #' @param perm_data \code{list} containing permutation data where length is the number of simulations.
--- R/fishers_method_functions.R
+++ R/fishers_method_functions.R
@@ -1,3 +1,13 @@
+#' Calculate Fishers method test statistic.
+#'
+#' @param gene_rank_statistic \code{vector} containing rank statistic of genes of interest.
+#' @export
+calculate_fishers_method_test_statistic <- function(gene_rank_statistic){
+  # Calculate test statistic
+  fishers_statistic <- -2*(sum(log(gene_rank_statistic)))
+  return(fishers_statistic)
+}
+
 #' Calculate empirical p-value for Fisher's method.
 #'
 #' \enumerate{
